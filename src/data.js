b4d286e9d5ea9e54cffa012862edb08367312286
Veselin803 <75386022+Veselin803@users.noreply.github.com>
2022-07-08 23:29:07 +0200

    Add files via upload
    dodati css, data.js sa objavama i apikacija sama

---
+++ src/data.js
@@ -0,0 +1,65 @@
+export const postInfo = [
+    {
+        id: 1,
+        headerTitle: 'Raspored filmova',
+        headerSubtitle: 'Ponedeljak',
+        headerText: 'Satnica filmova za ponedeljak.',
+        title: 'Raspored filmova',
+        description: 'PDF fajl sa rasporedom filmova',
+        img: 'https://www.pngmart.com/files/5/Movie-PNG-Transparent-Image.png'
+    },
+    {
+        id: 2,
+        headerTitle: 'Raspored filmova',
+        headerSubtitle: 'Utorak',
+        headerText: 'Satnica filmova za utorak.',
+        title: 'Raspored filmova',
+        description: 'PDF fajl sa rasporedom filmova',
+        img: 'https://www.pngmart.com/files/5/Movie-PNG-Transparent-Image.png'
+    },
+    {
+        id: 3,
+        headerTitle: 'Raspored filmova',
+        headerSubtitle: 'Sreda',
+        headerText: 'Satnica filmova za sredu.',
+        title: 'Raspored filmova',
+        description: 'PDF fajl sa rasporedom filmova',
+        img: 'https://www.pngmart.com/files/5/Movie-PNG-Transparent-Image.png'
+    },
+    {
+        id: 4,
+        headerTitle: 'Raspored filmova',
+        headerSubtitle: 'Četvrtak',
+        headerText: 'Satnica filmova za sredu.',
+        title: 'Raspored filmova',
+        description: 'PDF fajl sa rasporedom filmova',
+        img: 'https://www.pngmart.com/files/5/Movie-PNG-Transparent-Image.png'
+    },
+    {
+        id: 5,
+        headerTitle: 'Raspored filmova',
+        headerSubtitle: 'Petak',
+        headerText: 'Satnica filmova za petak.',
+        title: 'Raspored filmova',
+        description: 'PDF fajl sa rasporedom filmova',
+        img: 'https://www.pngmart.com/files/5/Movie-PNG-Transparent-Image.png'
+    },
+    {
+        id: 6,
+        headerTitle: 'Raspored filmova',
+        headerSubtitle: 'Subota',
+        headerText: 'Satnica filmova za subotu.',
+        title: 'Raspored filmova',
+        description: 'PDF fajl sa rasporedom filmova',
+        img: 'https://www.pngmart.com/files/5/Movie-PNG-Transparent-Image.png'
+    },
+    {
+        id: 7,
+        headerTitle: 'Raspored filmova',
+        headerSubtitle: 'Nedelja',
+        headerText: 'Satnica filmova za nedelju.',
+        title: 'Raspored filmova',
+        description: 'PDF fajl sa rasporedom filmova',
+        img: 'https://www.pngmart.com/files/5/Movie-PNG-Transparent-Image.png'
+    }
+];
